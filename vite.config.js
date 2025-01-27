e713e8c98d42c9f000404e90892847a79e5c593d
Yiwen Zhang <138088309+Yvonnezhang924@users.noreply.github.com>
2025-01-26 21:08:14 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Yvonnezhang924.github.io/', // 设置 base 为根路径，因为这是 GitHub Pages 用户/组织页面
+  base: "https://github.com/Yvonnezhang924/yvonnezhang924.github.io", // 设置 base 为根路径，因为这是 GitHub Pages 用户/组织页面
 })
